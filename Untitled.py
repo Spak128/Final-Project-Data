fc07107950a80c7c072a83b7c42b7e501a2422cc
Spak128 <104697631+Spak128@users.noreply.github.com>
2022-05-01 19:43:13 -0400

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[4]:
+
+
+m = 5
+c = -1
+x = [0,1,2,3,4,5,6]
+
+
+# In[5]:
+
+
+y = None
+
+
+# In[6]:
+
+
+print(y)
+
+
+# In[12]:
+
+
+x = np.array(x)
+y = m*x + c
+
+
+# In[13]:
+
+
+print(y)
+
+
+# In[14]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[15]:
+
+
+None
+
